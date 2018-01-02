9acdf229f7135c7651a16ec5c1a76eb2bfebfa13
jolavb <julianb@bu.edu>
2018-01-02 13:01:59 -0500

    Add innerpages and timeline

--- assets/scripts/bin/pageEvents.js
+++ assets/scripts/bin/pageEvents.js
@@ -2,11 +2,14 @@
 const intro = require('./../templates/innerpage/intro.handlebars')
 const grocery = require('./../templates/innerpage/grocery.handlebars')
 const tic_tac = require('./../templates/innerpage/tic_tac.handlebars')
+const jeb = require('./../templates/innerpage/jeb.handlebars')
+
 
 const views = {
   Intro: intro(),
   'Grocery-Basket': grocery(),
-  'Tic-Tac-Doh!': tic_tac()
+  'Tic-Tac-Doh!': tic_tac(),
+  'JEB! File Hosting': jeb()
 }
 
 const linksHandler = function () {
