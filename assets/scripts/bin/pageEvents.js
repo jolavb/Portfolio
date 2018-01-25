0a301f4949579eb5cd1698fb44194afc1b1a051a
jolavb <julianb@bu.edu>
2018-01-25 13:41:03 -0500

    Add reformatted front page
    1. Added Slider to front page with project info
    2. Added footer to  front page.

--- assets/scripts/bin/pageEvents.js
+++ assets/scripts/bin/pageEvents.js
@@ -1,13 +1,12 @@
 const d3Events = require('./../d3/index')
 
-
 const loadTabEvents = function () {
   $('.crime-data').on('click', d3Events.Crime)
   $('.iris-data').on('click', d3Events.Iris)
   $('.wine-data').on('click', d3Events.loadWineData)
   $('.thumbnail').click(function (e) {
     const selected = $(this).attr('aria-controls')
-    $('.active').removeClass('active')
+    $('.page-tab').removeClass('active')
     $('li > a[aria-controls=' + selected + ']').parent().addClass('active')
     // $(this).tab('show')
   })
